1ce7c928b5018864760584f1960887b93bdf552a
Sumit <sum.itvoid00@gmail.com>
2025-06-22 13:48:55 +0530

    used smaller model

--- backend/server.py
+++ backend/server.py
@@ -125,6 +125,8 @@ def handle_image_generation():
     return resp
 
 if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
 
